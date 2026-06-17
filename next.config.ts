30eacab7fd925265942d484b76177d27623e245a
Julia Tokarevskaia <j.linsher.jz@gmail.com>
2026-06-17 14:36:45 +0200

    Configure assets export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  unoptimized: true,
+  basePath: '/jt-web-portfolio',
 };
 
 export default nextConfig;
